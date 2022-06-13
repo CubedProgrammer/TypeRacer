b46bb7fef8014566764335394af8533acf362c32
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2022-06-13 21:49:58 +0000

    Added GPL.

--- server/race.c
+++ server/race.c
@@ -1,3 +1,14 @@
+// /CubedProgrammer/TypeRacer, a type racing game.
+// Copyright (C) 2022, github.com/CubedProgrammer owner of said account.
+
+// This file is part of /CubedProgrammer/TypeRacer.
+
+// /CubedProgrammer/TypeRacer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+// /CubedProgrammer/TypeRacer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with /CubedProgrammer/TypeRacer. If not, see <https://www.gnu.org/licenses/>.
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
